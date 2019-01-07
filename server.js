8b2e9d25c2b0a99e0d43a833ff95031c18acc9a7
Kyle Nickel <nickelkr@gmail.com>
2019-01-06 22:26:03 -0500

    Asset managment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen(3000, function() {
-  console.log('Server running on port 80...');
+connect().use(serveStatic(__dirname + '/dist')).listen(3000, function() {
+  console.log('Server running on port 3000...');
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import * as Phaser from 'phaser';
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'board');
+import Triangle from './assets/art/test-triangle.png';
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('triangle', Triangle);
+}
+
+function create() {
+  var triangle = this.add.sprite(80, 0, 'triangle');
+}
