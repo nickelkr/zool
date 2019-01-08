cd5bfe042ae1207418f4285d2e024e5de1bde285
Kyle Nickel <nickelkr@gmail.com>
2019-01-07 20:27:10 -0500

    10x10 board of triangles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Triangle from './assets/art/test-triangle.png';
 var config = {
   type: Phaser.AUTO,
   width: 800,
-  height: 600,
+  height: 700,
   scene: {
     preload: preload,
     create: create
@@ -19,5 +19,11 @@ function preload() {
 }
 
 function create() {
-  var triangle = this.add.sprite(80, 0, 'triangle');
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      let x = 35 + (i * 70);
+      let y = 35 + (j * 70);
+      this.add.sprite(x, y, 'triangle');
+    }
+  }
 }
